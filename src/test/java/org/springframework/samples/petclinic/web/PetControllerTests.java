8394439172cb51b46d484fc12fea37f2aed9de10
jmstechhome11 <87384358+jmstechhome11@users.noreply.github.com>
2021-08-04 21:31:39 +0530

    Update PetControllerTests.java

--- src/test/java/org/springframework/samples/petclinic/web/PetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/PetControllerTests.java
@@ -102,7 +102,7 @@ public class PetControllerTests {
     public void testProcessUpdateFormHasErrors() throws Exception {
         mockMvc.perform(post("/owners/{ownerId}/pets/{petId}/edit", TEST_OWNER_ID, TEST_PET_ID)
             .param("name", "Betty")
-            .param("birthDate", "2015/02/12")
+            .param("birthDate", "20156565/026565/12755")
         )
             .andExpect(model().attributeHasNoErrors("owner"))
             .andExpect(model().attributeHasErrors("pet"))
